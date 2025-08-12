5eaae8a158f7bcf42a3cfa14312de4f80df5bcf3
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-12 21:45:22 +0100

    Fix malli schema with :string

--- src/piotr_yuxuan/service_template/config.clj
+++ src/piotr_yuxuan/service_template/config.clj
@@ -16,11 +16,11 @@
                 :short-option "-h"
                 :arg-number 0}]]
 
-    [::db/hostname [string? {:default "localhost", :long-option "--db-hostname"}]]
+    [::db/hostname [:string {:default "localhost", :long-option "--db-hostname"}]]
     [::db/port [pos-int? {:default 5432, :long-option "--db-port"}]]
-    [::db/dbname [string? {:default "database", :long-option "--dbname"}]]
-    [::db/username [string? {:default "user", :long-option "--db-username"}]]
-    [::db/password [string? {:default "password"
+    [::db/dbname [:string {:default "database", :long-option "--dbname"}]]
+    [::db/username [:string {:default "user", :long-option "--db-username"}]]
+    [::db/password [:string {:default "password"
                              :env-var "DB_PASSWORD"
                              :secret true}]]
     [::db/migrate? [boolean? {:default false, :long-option "--db-migrate"}]]]))
