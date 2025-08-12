e451955493fe91c19048b006e571dce676484276
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-12 19:51:08 +0100

    Starling API op: get-accounts

---
+++ src/piotr_yuxuan/service_template/starling_api.clj
@@ -0,0 +1,37 @@
+(ns piotr-yuxuan.service-template.starling-api
+  (:require
+   [clj-http.client :as http]
+   [jsonista.core :as j]
+   [malli.core :as m]
+   [malli.transform :as mt]
+   [piotr-yuxuan.service-template.entity :as entity]
+   [reitit.ring.malli]
+   [safely.core :refer [safely]]))
+
+(defn request->response
+  [request]
+  (safely (http/request request)
+    :on-error
+    :retry-delay [:random-exp-backoff :base 300 :+/- 0.35 :max 25000]
+    :max-retries 5
+    :circuit-breaker ::api))
+
+(def GetAccountResponse
+  (m/schema
+   [:map {:closed true}
+    [:accounts
+     [:sequential entity/Account]]]))
+
+(def GetAccountResponse-body-decoder
+  (m/decoder GetAccountResponse mt/json-transformer))
+
+(defn get-accounts
+  [{:keys [token]}]
+  (-> (request->response
+       {:method :get
+        :url "https://api-sandbox.starlingbank.com/api/v2/accounts"
+        :headers {"accept" "application/json"
+                  "authorization" (str "Bearer " token)}})
+      :body
+      (jsonista.core/read-value jsonista.core/keyword-keys-object-mapper)
+      GetAccountResponse-body-decoder))
