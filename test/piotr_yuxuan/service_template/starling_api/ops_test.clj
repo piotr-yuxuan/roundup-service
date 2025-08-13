a758a06f3d065b2c3f809c908dd8475d3ce20088
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-13 20:43:47 +0100

    Test ops/get-accounts

--- test/piotr_yuxuan/service_template/starling_api/ops_test.clj
+++ test/piotr_yuxuan/service_template/starling_api/ops_test.clj
@@ -1,20 +1,43 @@
 (ns piotr-yuxuan.service-template.starling-api.ops-test
   (:require
-   [clojure.test :refer [deftest is]]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is testing]]
+   [malli.core :as m]
    [malli.generator :as mg]
-   [piotr-yuxuan.service-template.starling-api.ops :as ops]))
+   [malli.transform :as mt]
+   [piotr-yuxuan.service-template.http :as st.http]
+   [piotr-yuxuan.service-template.railway :refer [error ok]]
+   [piotr-yuxuan.service-template.starling-api.entity :as entity]
+   [piotr-yuxuan.service-template.starling-api.ops :as ops]
+   [ring.util.http-status :as http-status]))
 
 (deftest get-accounts-test
-  (doseq [response-body (take 15 (mg/generate ops/GetAccountResponse))]
-    (with-redefs [ops/request->response
-                  (fn [req]
-                    ;; capture request for assertions
-                    (is (= :get (:method req)))
-                    (is (re-find #"localhost:80/v2/accounts" (:url req)))
-                    (is (contains? (:headers req) "authorization") "No authorization header.")
-                    (is (= "Bearer my-token" (get-in req [:headers "authorization"])) "Incorrect authorization token")
-                    {:body response-body})]
-      (let [accounts (ops/get-accounts
-                      {::ops/api-base "http://localhost:80"}
-                      {:token "my-token"})]
-        (is (= (-> response-body :accounts) accounts))))))
+  (testing "happy path"
+    (let [token (mg/generate [:string {:min 10 :max 15}])
+          api-base (mg/generate [:string {:min 10 :max 15}])
+          expected-request {:method :get
+                            :url (str/join "/" [api-base "v2/accounts"])
+                            :headers {"accept" "application/json"
+                                      "authorization" (str "Bearer " token)}}
+          expected-account (mg/generate entity/Account)]
+      (with-redefs [st.http/-request->response (fn [request]
+                                                 (is (= expected-request request))
+                                                 {:status http-status/ok
+                                                  :body {:accounts [(m/encode entity/Account expected-account mt/json-transformer)]}})]
+        (is (= (ok {:accounts [expected-account]})
+               (ops/get-accounts
+                {::ops/api-base api-base}
+                {:token token}))))))
+  (testing "auth issue"
+    (let [token (mg/generate [:string {:min 10 :max 15}])
+          api-base (mg/generate [:string {:min 10 :max 15}])]
+      (with-redefs [st.http/-request->response (constantly
+                                                {:status http-status/forbidden
+                                                 :body {:error_description "Could not validate provided access token"
+                                                        :error "invalid_token"}})]
+        (is (= (error {:body {:error_description "Could not validate provided access token",
+                              :error "invalid_token"},
+                       :status 403})
+               (ops/get-accounts
+                {::ops/api-base api-base}
+                {:token token})))))))
