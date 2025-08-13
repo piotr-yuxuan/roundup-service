477a691270844e0945a59c690da65db0ef683536
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-13 11:06:04 +0100

    Add docstring and pointers for railway-style programming

--- src/piotr_yuxuan/service_template/railway.clj
+++ src/piotr_yuxuan/service_template/railway.clj
@@ -1,5 +1,6 @@
 (ns piotr-yuxuan.service-template.railway
-  "https://fsharpforfunandprofit.com/rop/")
+  "Inspired by https://fsharpforfunandprofit.com/rop/.
+   Simpler than https://github.com/fmnoise/flow.")
 
 (defn ok [v] {:ok v})
 (defn error [err] {:error err})
