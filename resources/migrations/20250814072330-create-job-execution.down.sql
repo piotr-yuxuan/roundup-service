806d215cf2770ed3a3293485e7c9d0b49bc8bfa2
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-14 13:44:21 +0100

    Add migrations to shape up the roundup_job_execution table

--- resources/migrations/20250814072330-create-job-execution.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE roundup_job_execution;
--- resources/migrations/20250814072330-create-job-execution.up.sql
+++ resources/migrations/20250814072330-create-job-execution.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE roundup_job_execution (
     week_start_date DATE NOT NULL,
     account_uid UUID,
     savings_goal_uid UUID,
-    round_up_amount_in_minor_units NUMERIC(12, 0)
+    round_up_amount_in_minor_units NUMERIC(12, 0),
 
     CONSTRAINT unique_week_per_account UNIQUE (week_start_date, account_uid)
 );
