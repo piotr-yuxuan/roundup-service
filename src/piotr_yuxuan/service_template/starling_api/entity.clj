24fab714b426f0c70463b13fd2b8e065e0b1abe1
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-08-15 20:13:37 +0100

    Document starling-api.entity

--- src/piotr_yuxuan/service_template/starling_api/entity.clj
+++ src/piotr_yuxuan/service_template/starling_api/entity.clj
@@ -1,10 +1,11 @@
 (ns piotr-yuxuan.service-template.starling-api.entity
-  "Only keep the attributes that are interesting to us."
+  "Entities for Starling API, keeping only relevant attributes."
   (:require
    [malli.core :as m]
    [piotr-yuxuan.service-template.math :refer [NonNegInt64]]))
 
 (def Currency
+  "Supported currency codes"
   (m/schema
    [:enum "UNDEFINED" "AED" "AFN" "ALL" "AMD" "ANG" "AOA" "ARS" "AUD" "AWG" "AZN" "BAM" "BBD" "BDT" "BGN" "BHD" "BIF" "BMD" "BND" "BOB" "BOV" "BRL" "BSD" "BTN" "BWP" "BYN" "BYR" "BZD" "CAD" "CDF" "CHE" "CHF" "CHW" "CLF" "CLP" "CNY" "COP" "COU" "CRC" "CUC" "CUP" "CVE" "CZK" "DJF" "DKK" "DOP" "DZD" "EGP" "ERN" "ETB" "EUR" "FJD" "FKP" "GBP" "GEL" "GHS" "GIP" "GMD" "GNF" "GTQ" "GYD" "HKD" "HNL" "HRK" "HTG" "HUF" "IDR" "ILS" "INR" "IQD" "IRR" "ISK" "JMD" "JOD" "JPY" "KES" "KGS" "KHR" "KMF" "KPW" "KRW" "KWD" "KYD" "KZT" "LAK" "LBP" "LKR" "LRD" "LSL" "LTL" "LYD" "MAD" "MDL" "MGA" "MKD" "MMK" "MNT" "MOP" "MRO" "MRU" "MUR" "MVR" "MWK" "MXN" "MXV" "MYR" "MZN" "NAD" "NGN" "NIO" "NOK" "NPR" "NZD" "OMR" "PAB" "PEN" "PGK" "PHP" "PKR" "PLN" "PYG" "QAR" "RON" "RSD" "RUB" "RUR" "RWF" "SAR" "SBD" "SCR" "SDG" "SEK" "SGD" "SHP" "SLL" "SLE" "SOS" "SRD" "SSP" "STD" "STN" "SVC" "SYP" "SZL" "THB" "TJS" "TMT" "TND" "TOP" "TRY" "TTD" "TWD" "TZS" "UAH" "UGX" "USD" "USN" "USS" "UYI" "UYU" "UZS" "VEF" "VES" "VND" "VUV" "WST" "XAF" "XAG" "XAU" "XBA" "XBB" "XBC" "XBD" "XCD" "XCG" "XDR" "XOF" "XPD" "XPF" "XPT" "XSU" "XTS" "XUA" "XXX" "YER" "ZAR" "ZMW" "ZWL" "ZWG"]))
 
@@ -26,6 +27,7 @@
     [:minorUnits NonNegInt64]]))
 
 (def FeedItem
+  "Represent a transaction feed item"
   (m/schema
    [:map {:closed true}
     [:amount CurrencyAndAmount]
